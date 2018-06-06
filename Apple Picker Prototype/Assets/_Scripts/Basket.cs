cf04fddc854923a4fe18d2635f82cdc35cc7ee55
sankmish <mishra.sancho@gmail.com>
2018-06-05 22:31:35 -0400

    Apple Picker Moving Baskets, Catching Apples, And Raised Difficuluty
    Changed Basket Script to follow mouse movement and added collision check with apples and destroyed them. Created a duplicate proto scene with raised difficulty parameters.

--- Apple Picker Prototype/Assets/_Scripts/Basket.cs
+++ Apple Picker Prototype/Assets/_Scripts/Basket.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Basket : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +10,21 @@ public class Basket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
 	}
+
+    void OnCollisionEnter( Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith = coll.gameObject)
+        {
+            Destroy(collidedWith);
+        }
+    } 
+
 }
